1069a62e693fa8b17292f88c942917bbcf9b03c4
kumuthu-vinsura <69743824+kumuthu-vinsura@users.noreply.github.com>
2021-01-26 00:51:03 +0530

    cant send null message option added. Auto resizing elements added. Online user count added

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ const socket = io('http://localhost:3000')
 const messageForm = document.getElementById('send-container')
 const messageContainer = document.getElementById('message-container')
 const messageInput = document.getElementById('message-input')
+const onlineCount = document.getElementById('online')
 
-const nameofuser = prompt('What is your good name ?') 
+const nameofuser = prompt('What is your good name ?', "Noobie") 
 
 appendMassage('You Joind', 'BOT Message')
 socket.emit('new-user', nameofuser )
+
+socket.on('users-online', numofusers =>{
+    onlineCount.innerHTML=numofusers
+})
 
 socket.on('chat-message', data =>{
     appendMassage(data.message, data.nameofuser)
@@ -19,16 +24,23 @@ socket.on('user-connected', nameofuser =>{
     window.scrollTo(0, messageContainer.scrollHeight)
 })
 
+socket.on('user-disconnected', nameofuser =>{
+    appendMassage(nameofuser+' left the chat', 'BOT Message' )
+    window.scrollTo(0, messageContainer.scrollHeight)
+})
+
 
 
 messageForm.addEventListener('submit', e=>{
     e.preventDefault()
     const message = messageInput.value
-    appendMassageMe(message)
-    window.focus()
-    window.scrollTo(0, messageContainer.scrollHeight)
-    socket.emit('send-chat-message',message)
-    messageInput.value = ''
+    if(message.trim() != ''){
+        appendMassageMe(message)
+        window.focus()
+        window.scrollTo(0, messageContainer.scrollHeight)
+        socket.emit('send-chat-message',message)
+        messageInput.value = ''
+    }
     messageInput.focus()
 })
 
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const io = require('socket.io')(3000)
 const users = {}
 
+let numofusers = 0;
+
 io.on('connection', socket =>{
     console.log('New User Landed')
     socket.on('new-user', nameofuser =>{
         users[socket.id] = nameofuser
+        numofusers++
         socket.broadcast.emit('user-connected',nameofuser)
+        io.emit('users-online',numofusers)
     })
+
+    socket.on('disconnect', () =>{
+        socket.broadcast.emit('user-disconnected',users[socket.id])
+        delete users[socket.id]
+        numofusers--
+        io.emit('users-online',numofusers)
+    })
+
     socket.on('send-chat-message', message => {
         socket.broadcast.emit('chat-message', {message: message, nameofuser: users[socket.id]})
     })
